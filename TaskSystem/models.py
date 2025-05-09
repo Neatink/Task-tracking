f9c9c45632c01a4c1a87f76edb11ee6274565d50
vladislavchik42 <minyaylo51@gmail.com>
2025-05-09 19:54:56 +0300

    Rename 'date' to 'create_date',add 'auto_now_add'

--- TaskSystem/models.py
+++ TaskSystem/models.py
@@ -11,6 +11,7 @@ class TaskStatus(models.Model):
 
 class TaskPriority(models.Model):
     name = models.CharField(max_length=35)
+    
     def __str__(self):
         return self.name
     
@@ -20,7 +21,7 @@ class Task(models.Model):
     description = models.TextField(max_length=200)
     status = models.ForeignKey(TaskStatus,max_length=50,on_delete=models.SET_NULL,null=True)
     priority = models.ForeignKey(TaskPriority,max_length=35,on_delete=models.SET_NULL,null=True)
-    date = models.DateField()
+    create_date = models.DateField(auto_now_add=True)
     user = models.ForeignKey(User,on_delete=models.CASCADE)
     
     def __str__(self):
