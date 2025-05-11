a131b2a3bcb9531f41a21d01ff9fb09531b4d49c
vladislavchik42 <minyaylo51@gmail.com>
2025-05-11 20:21:49 +0300

    Add templates(task_priorities,task_statuses,tasks) and views

--- TaskSystem/views.py
+++ TaskSystem/views.py
@@ -1,3 +1,21 @@
-from django.shortcuts import render
+from django.http import HttpResponse
+from django.views.generic import TemplateView,ListView
+from .models import Task,TaskPriority,TaskStatus
 
-# Create your views here.
+
+class TasksView(ListView):
+    context_object_name = 'tasks'
+    template_name = "tasks.html"
+    model = Task
+
+
+class TaskPrioritiesView(ListView):
+    context_object_name = 'taskprior'
+    template_name = "task_priorities.html"
+    model = TaskPriority
+
+
+class TaskStatusesView(ListView):
+    context_object_name = 'taskstatus'
+    template_name = "task_statuses.html"
+    model = TaskStatus
